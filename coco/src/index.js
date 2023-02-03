4e903d1df174bbbc17e34cdd70d9bf978e591f4e
Jiseok Kim <86154028+p1ants@users.noreply.github.com>
2023-02-03 21:44:52 +0900

    초기세팅(page구성...)

--- coco/src/index.js
+++ coco/src/index.js
@@ -1,14 +1,44 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import './index.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+// import { BrowserRouter as Router, Route } from "react-router-dom";
+//page 임포트
+import App from './App';
+import NotFound from './pages/NotFound';
+import Login from './pages/Login';
+import Main from './pages/Main';
+import Detail from './pages/Detail';
+
+// 리덕스, 스토어, 쿠키,
+// import { CookiesProvider } from 'react-cookie';
+import { Provider } from 'react-redux';
+import store from './redux/config/configStore';
+import axios from 'axios';
+axios.defaults.withCredentials = true;
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <NotFound />,
+    children: [
+      { index: true, element: <Login /> },
+      { path: 'Main', element: <Main /> },
+      { path: 'Detail/:Id', element: <Detail /> },
+    ],
+  },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Provider store={store}>
+    <React.StrictMode>
+      {/* <CookiesProvider> */}
+      <RouterProvider router={router} />
+      {/* </CookiesProvider> */}
+    </React.StrictMode>
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
