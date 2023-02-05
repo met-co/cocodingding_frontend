9189f2ae959b4ccdf84d144330fc3611c0687e64
Jiseok Kim <86154028+p1ants@users.noreply.github.com>
2023-02-05 18:34:40 +0900

    모달 라이브러리 설치.

--- coco/src/pages/Main.jsx
+++ coco/src/pages/Main.jsx
@@ -1,17 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
 import CreateRoomForm from '../components/Main/CreateRoomForm';
 
 function Main() {
+  // rooms 데이터를 가져옴
   const rooms = useSelector((state) => state.rooms);
+  // 모달 상태를 관리하는 showModal state 선언
+  const [showModal, setShowModal] = useState(false);
 
   return (
     <div>
-      <CreateRoomForm />
+      {/* 버튼 클릭시 setShowModal(true) 실행 */}
+      <button onClick={() => setShowModal(true)}>방만들기</button>
+      {/* showModal 값이 true 일 때 CreateRoomForm 컴포넌트 렌더링 */}
+      {showModal && <CreateRoomForm onClose={() => setShowModal(false)} />}
       <ul>
         {rooms.map((room) => (
           <li key={room.id}>
-            <a href={`/${room.id}`}>{room.name}</a>
+            <a href={`${room.id}`}>{room.name}</a>
           </li>
         ))}
       </ul>
