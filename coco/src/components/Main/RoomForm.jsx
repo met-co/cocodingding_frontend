971b6e3b659d2b7e991d522236f3b7367a8778b8
hye seong <kanekasumi@gmail.com>
2023-02-08 16:09:38 +0900

    feat: 메인페이지 작업중

--- coco/src/components/Main/RoomForm.jsx
+++ coco/src/components/Main/RoomForm.jsx
@@ -1,39 +1,40 @@
-import axios from 'axios';
-import React, { useEffect, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import styled from 'styled-components';
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
 // RoomForm 컴포넌트에서 rooms state 및 rooms 데이터 가져오는 기능 구현
 export default function RoomForm() {
   // rooms 상태정의, setRooms 함수 정의
   const [rooms, setRooms] = useState([]);
-  const [search, setSearch] = useState('');
+  const [search, setSearch] = useState("");
   const [filteredRooms, setFilteredRooms] = useState([]);
   const navigate = useNavigate();
-  useEffect(() => {
-    async function fetchRooms() {
-      // localhost:3001/rooms에서 데이터 가져오기
-      const response = await axios.get('http://localhost:3001/rooms');
-      // 가져온 데이터를 rooms 상태에 넣기
-      setRooms(response.data);
-      console.log(response);
-    }
-    // 함수 호출
-    fetchRooms();
-  }, []);
 
-  useEffect(() => {
-    setFilteredRooms(
-      rooms.filter((room) =>
-        room.roomName.toLowerCase().includes(search.toLowerCase())
-      )
-    );
-  }, [search, rooms]);
+  // useEffect(() => {
+  //   async function fetchRooms() {
+  //     // localhost:3001/rooms에서 데이터 가져오기
+  //     const response = await axios.get('http://localhost:3001/rooms');
+  //     // 가져온 데이터를 rooms 상태에 넣기
+  //     setRooms(response.data);
+  //     console.log(response);
+  //   }
+  //   // 함수 호출
+  //   fetchRooms();
+  // }, []);
+
+  // useEffect(() => {
+  //   setFilteredRooms(
+  //     rooms.filter((room) =>
+  //       room.roomName.toLowerCase().includes(search.toLowerCase())
+  //     )
+  //   );
+  // }, [search, rooms]);
 
   return (
     <div>
       <input
-        type='text'
-        placeholder='Search rooms'
+        type="text"
+        placeholder="Search rooms"
         value={search}
         onChange={(e) => setSearch(e.target.value)}
       />
