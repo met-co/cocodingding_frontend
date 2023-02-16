62dad761716f07bf094b1723fa53c272d1b4d3af
Jiseok Kim <86154028+p1ants@users.noreply.github.com>
2023-02-16 11:40:48 +0900

    jn

--- coco/src/redux/modules/chatSlice.js
+++ coco/src/redux/modules/chatSlice.js
@@ -1,61 +1,61 @@
-import { createSlice } from '@reduxjs/toolkit';
-import { io } from 'socket.io-client';
+// import { createSlice } from '@reduxjs/toolkit';
+// import { io } from 'socket.io-client';
 
-const ENDPOINT = 'https://cocodingding.shop/';
+// const ENDPOINT = 'https://cocodingding.shop/';
 
-export const chatSlice = createSlice({
-  name: 'chat',
-  initialState: {
-    chatRoom: null,
-    messages: [],
-  },
-  reducers: {
-    setChatRoom: (state, action) => {
-      state.chatRoom = action.payload;
-    },
-    addMessage: (state, action) => {
-      state.messages.push(action.payload);
-    },
-  },
-});
+// export const chatSlice = createSlice({
+//   name: 'chat',
+//   initialState: {
+//     chatRoom: null,
+//     messages: [],
+//   },
+//   reducers: {
+//     setChatRoom: (state, action) => {
+//       state.chatRoom = action.payload;
+//     },
+//     addMessage: (state, action) => {
+//       state.messages.push(action.payload);
+//     },
+//   },
+// });
 
-export const { setChatRoom } = chatSlice.actions;
+// export const { setChatRoom } = chatSlice.actions;
 
-export const selectChatRoom = (state) => state.chat.chatRoom;
+// export const selectChatRoom = (state) => state.chat.chatRoom;
 
-export default chatSlice.reducer;
+// export default chatSlice.reducer;
 
-export const joinRoom = (roomId) => (dispatch, getState) => {
-  const socket = io(ENDPOINT);
-  dispatch(setChatRoom({ id: roomId, socket }));
-  const { chatRoom } = getState().chat;
-  if (chatRoom.socket) {
-    chatRoom.socket.connect();
-    chatRoom.socket.emit('join', roomId);
-  }
-};
+// export const joinRoom = (roomId) => (dispatch, getState) => {
+//   const socket = io(ENDPOINT);
+//   dispatch(setChatRoom({ id: roomId, socket }));
+//   const { chatRoom } = getState().chat;
+//   if (chatRoom.socket) {
+//     chatRoom.socket.connect();
+//     chatRoom.socket.emit('join', roomId);
+//   }
+// };
 
-export const leaveRoom = () => (dispatch, getState) => {
-  const { chatRoom } = getState().chat;
-  if (chatRoom.socket) {
-    chatRoom.socket.emit('leave');
-    chatRoom.socket.disconnect();
-  }
-  dispatch(setChatRoom(null));
-};
+// export const leaveRoom = () => (dispatch, getState) => {
+//   const { chatRoom } = getState().chat;
+//   if (chatRoom.socket) {
+//     chatRoom.socket.emit('leave');
+//     chatRoom.socket.disconnect();
+//   }
+//   dispatch(setChatRoom(null));
+// };
 
-export const receiveMessage = () => (dispatch, getState) => {
-  const { chatRoom } = getState().chat;
-  if (chatRoom.socket) {
-    chatRoom.socket.on('message', (message) => {
-      dispatch(addMessage(message));
-    });
-  }
-};
+// export const receiveMessage = () => (dispatch, getState) => {
+//   const { chatRoom } = getState().chat;
+//   if (chatRoom.socket) {
+//     chatRoom.socket.on('message', (message) => {
+//       dispatch(addMessage(message));
+//     });
+//   }
+// };
 
-export const sendMessage = (data) => (dispatch, getState) => {
-  const { chatRoom } = getState().chat;
-  if (chatRoom.socket) {
-    chatRoom.socket.emit('message', data.message);
-  }
-};
+// export const sendMessage = (data) => (dispatch, getState) => {
+//   const { chatRoom } = getState().chat;
+//   if (chatRoom.socket) {
+//     chatRoom.socket.emit('message', data.message);
+//   }
+// };
