38884aabf1879464fb631fd7f63581c8ed9ff541
Jiseok Kim <86154028+p1ants@users.noreply.github.com>
2023-03-09 10:47:35 +0900

    메인 조금 수정

--- coco/src/components/Main/SearchBar.jsx
+++ coco/src/components/Main/SearchBar.jsx
@@ -1,15 +1,15 @@
-import React from "react";
-import styled from "styled-components";
+import React from 'react';
+import styled from 'styled-components';
 
 export default function SearchBar({ search, setSearch, setCategory, rooms }) {
   const handleSearchChange = (e) => {
     setSearch(e.target.value);
-    setCategory("");
+    setCategory('');
   };
 
   const selectCategory = (category) => {
     setCategory(category);
-    setSearch("");
+    setSearch('');
   };
 
   const categories = Array.from(new Set(rooms.map((room) => room.category)));
@@ -17,8 +17,8 @@ export default function SearchBar({ search, setSearch, setCategory, rooms }) {
   return (
     <StBackground>
       <StInput
-        type="text"
-        placeholder="  참여하고싶은 방을 찾아보세요"
+        type='text'
+        placeholder='  참여하고싶은 방을 찾아보세요'
         value={search}
         onChange={handleSearchChange}
       />
@@ -36,7 +36,7 @@ export default function SearchBar({ search, setSearch, setCategory, rooms }) {
 
 const StBackground = styled.div`
   background-color: #ffe45c;
-  width: 500px;
+  width: 550px;
   flex-basis: 60%;
   /* max-width: 105vw; */
   display: flex;
